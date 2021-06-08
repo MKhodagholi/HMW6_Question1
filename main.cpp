061098cea2d6c45eacb9282cac4bc2e616621be9
Mohammad <MKhodaagholi@gmail.com>
2021-06-08 16:26:24 +0430

    [try to solve the q1]

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Animal.h"
+
+using namespace std;
 
 int main() {
+    int num;
+    cout << "Please enter the number of animals: ";
+    cin >> num;
+    Animal array_animal[num];
+    cout << array_animal[0].getX();
 
     return 0;
 }
